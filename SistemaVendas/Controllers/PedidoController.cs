fe68c64d5748d85d6781b87131255cbbf30a2ee7
Matheus Balan <matheusbalanj5@gmail.com>
2023-01-18 15:01:02 -0300

    Classe Pedido

---
+++ SistemaVendas/Controllers/PedidoController.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.Repository;
+using SistemaVendas.Dto;
+using SistemaVendas.Models;
+using SistemaVendas.Context;
+
+namespace SistemaVendas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        private readonly PedidoRepository _repository;
+
+        public PedidoController(PedidoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(CadastrarPedidoDTO dto)
+        {
+            var pedido = new Pedido(dto);
+            _repository.Cadastrar(pedido);
+            return Ok(pedido);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var pedido = _repository.ObterPorId(id);
+
+            if(pedido is not null)
+            {
+                return Ok(pedido);
+            }
+            else
+            {
+                return NotFound(new { Mensagem = "Pedido não encontrado"});
+            }
+        } 
+        
+    }
+}
