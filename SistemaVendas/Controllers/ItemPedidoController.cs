d98b866369af50a43038d0fcb1fb5950861538b5
Matheus Balan <matheusbalanj5@gmail.com>
2023-02-02 16:02:30 -0300

    Pedidos quase completa (Falta o Delete)

---
+++ SistemaVendas/Controllers/ItemPedidoController.cs
@@ -0,0 +1,81 @@
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.Repository;
+using SistemaVendas.Dto;
+using SistemaVendas.Models;
+using SistemaVendas.Context;
+
+namespace SistemaVendas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class ItemPedidoController : ControllerBase
+    {
+        private readonly ItemPedidoRepository _repository;
+
+        public ItemPedidoController(ItemPedidoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(CadastrarItemPedidoDTO dto)
+        {
+            var item = new ItemPedido(dto);
+            _repository.Cadastrar(item);
+            return Ok(item);
+        }
+        
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            var item = _repository.Listar();
+
+            if(item is not null)
+            {
+                return Ok(item);
+            }
+            else
+            {
+                return NotFound(new { Mensagem = "Não há itens"});
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ConsultarPorId(int id)
+        {
+            var item = _repository.ObterPorId(id);
+
+            if(item is not null)
+            {
+                return Ok(item);
+            }
+            else
+            {
+                return NotFound(new { Mensagem = "Item não encontrado"});
+            }
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, AtualizarItemPedidoDTO dto)
+        {
+            var item = _repository.ObterPorId(id);
+
+            if(item is not null)
+            {
+                item.MapearAtualizarItemPedidoDTO(dto);
+                _repository.AtualizarItemPedido(item);
+                return Ok(item);
+            }
+            else
+            {
+                return NotFound(new { Mensagem = "Item não encontrado ou não existente"});
+            }
+        }
+    }
+}
---
+++ SistemaVendas/Repository/ItemPedidoRepository.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaVendas.Context;
+using SistemaVendas.Models;
+using SistemaVendas.Dto;
+
+namespace SistemaVendas.Repository
+{
+    public class ItemPedidoRepository
+    {
+        private readonly VendasContext _context;
+
+        public ItemPedidoRepository(VendasContext context)
+        {
+            _context = context;
+        }
+
+        public ItemPedido Cadastrar(ItemPedido item)
+        {
+            _context.ItensPedido.Add(item);
+            _context.SaveChanges();
+
+            return item;
+        }
+
+        public List<ItemPedido> Listar()
+        {
+            var item = _context.ItensPedido.ToList();
+            return item;
+        }
+
+        public ItemPedido ObterPorId(int id)
+        {
+            var item = _context.ItensPedido.Include(x => x.Servico)
+                                           .Include(x => x.Pedido)
+                                           .FirstOrDefault(x => x.Id == id);
+            return item; 
+        }
+
+        public ItemPedido AtualizarItemPedido(ItemPedido item)
+        {
+            _context.ItensPedido.Update(item);
+            _context.SaveChanges();
+
+            return item;
+        }
+        
+
+    }
+}
